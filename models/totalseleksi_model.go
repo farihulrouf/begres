9f07dfcdd50ccb56e1afe47527ab99290969a3dd
Farihul Rouf <farihulrouf@gmail.com>
2022-11-17 18:28:39 +0700

    seleksi

--- models/totalseleksi_model.go
+++ models/totalseleksi_model.go
@@ -3,7 +3,7 @@ package models
 //import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Totalseleksi struct {
-	Name      string  `json:"name,omitempty" validate:"required"`
+	Tender    string  `json:"tender,omitempty" validate:"required"`
 	Total     float32 `json:"total,omitempty" validate:"required"`
 	Totalpagu float32 `json:"totalpagu,omitempty" validate:"required"`
 	Pdn       float32 `json:"pdn,omitempty" validate:"required"`
