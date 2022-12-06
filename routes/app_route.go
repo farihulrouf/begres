f42cf21b4805a2cfb32a03c5eb5f803bdb65f6dc
Farihul Rouf <farihulrouf@gmail.com>
2022-12-06 20:50:44 +0700

    fix auth

--- routes/app_route.go
+++ routes/app_route.go
@@ -19,37 +19,37 @@ func AppRoute(app *fiber.App) {
 	app.Get("/api/pagus/filter", middleware.Authentication, controllers.GetAllFilter)
 	app.Get("/api/pagus/:paguId", middleware.Authentication, controllers.GetPagu)
 	app.Delete("/api/pagus/:paguId", middleware.Authentication, controllers.DeletePagu)
-	app.Put("/api/pagus/:paguId", controllers.EditPagu)
+	app.Put("/api/pagus/:paguId", middleware.Authentication, controllers.EditPagu)
 	app.Put("/api/pagus/edit/:paguId", middleware.Authentication, controllers.EditPaguUpload)
 	//EditPaguUpload
 
 	//Pagu anggaran
-	app.Post("/api/anggaran", controllers.CreateAnggaran)
-	app.Get("/api/anggaran", controllers.GetAllAnggaran)
-	app.Get("/api/anggaran/pagu/:paguId", controllers.GetFilterAnggaran)
-	app.Delete("/api/anggaran/:paguId", controllers.DeleteAnggran)
-	app.Put("/api/anggaran/:paguId", controllers.EditAnggaran)
-	app.Get("/api/anggaran/:paguId", controllers.GetAnggaran)
+	app.Post("/api/anggaran", middleware.Authentication, controllers.CreateAnggaran)
+	app.Get("/api/anggaran", middleware.Authentication, controllers.GetAllAnggaran)
+	app.Get("/api/anggaran/pagu/:paguId", middleware.Authentication, controllers.GetFilterAnggaran)
+	app.Delete("/api/anggaran/:paguId", middleware.Authentication, controllers.DeleteAnggran)
+	app.Put("/api/anggaran/:paguId", middleware.Authentication, controllers.EditAnggaran)
+	app.Get("/api/anggaran/:paguId", middleware.Authentication, controllers.GetAnggaran)
 
 	//langsung
-	app.Post("/api/langsung", controllers.CreateLangsung)
-	app.Get("/api/langsung", controllers.GetAllLangsung)
-	app.Get("/api/langsung/pagu/:paguId", controllers.GetFilterLangsung)
-	app.Get("/api/langsung/pagu/:paguId/:tipe", controllers.GetFilterLangsungByType)
-	app.Get("/api/langsung/:paguId", controllers.GetLangsung)
-	app.Get("/api/langsung/pagu/total/:paguId/:tipe", controllers.GetTotalTenderLangsung)
-	app.Get("/api/langsung/totalsemua/:paguId", controllers.GetAllTotalTenderLangsung)
+	app.Post("/api/langsung", middleware.Authentication, controllers.CreateLangsung)
+	app.Get("/api/langsung", middleware.Authentication, controllers.GetAllLangsung)
+	app.Get("/api/langsung/pagu/:paguId", middleware.Authentication, controllers.GetFilterLangsung)
+	app.Get("/api/langsung/pagu/:paguId/:tipe", middleware.Authentication, controllers.GetFilterLangsungByType)
+	app.Get("/api/langsung/:paguId", middleware.Authentication, controllers.GetLangsung)
+	app.Get("/api/langsung/pagu/total/:paguId/:tipe", middleware.Authentication, controllers.GetTotalTenderLangsung)
+	app.Get("/api/langsung/totalsemua/:paguId", middleware.Authentication, controllers.GetAllTotalTenderLangsung)
 
-	app.Get("/api/langsung/totalseleksitender/:paguId", controllers.GetAllTotalTenderLangsungBySeleksiCepat)
+	app.Get("/api/langsung/totalseleksitender/:paguId", middleware.Authentication, controllers.GetAllTotalTenderLangsungBySeleksiCepat)
 	//GetAllTotalTenderCepatAllSeleksi
-	app.Get("/api/jumlahtotalseleksi", controllers.GetAllTotalTenderCepatAllSeleksi)
-	app.Get("/api/jumlahtotal", controllers.GetAllTotalTenderLangsungAll)
-	app.Get("/api/totalpdn/:paguId", controllers.GetAllTotalTenderPdnAll)
+	app.Get("/api/jumlahtotalseleksi", middleware.Authentication, controllers.GetAllTotalTenderCepatAllSeleksi)
+	app.Get("/api/jumlahtotal", middleware.Authentication, controllers.GetAllTotalTenderLangsungAll)
+	app.Get("/api/totalpdn/:paguId", middleware.Authentication, controllers.GetAllTotalTenderPdnAll)
 
 	//app.Get("/api/jumlahratapdn/:paguId", controllers.GetAllTotalTenderPdnLookup)
 	//GetAllTotalTenderPdnLookup
-	app.Delete("/api/langsung/:paguId", controllers.DeleteLangsung)
-	app.Put("/api/langsung/:paguId", controllers.EditLangsug)
+	app.Delete("/api/langsung/:paguId", middleware.Authentication, controllers.DeleteLangsung)
+	app.Put("/api/langsung/:paguId", middleware.Authentication, controllers.EditLangsug)
 
 	//uploudfile
 	app.Post("/api/uploud", controllers.CreatetoUpload)
